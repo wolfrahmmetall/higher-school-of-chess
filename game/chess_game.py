7753bcfcd7fb1c5889e36d30dc9b6d9688a8840f
Ainur__lightning-edu <alsarimov@edu.hse.ru>
2024-11-06 18:01:37 +0300

    added game_over method and updated move method

--- game/chess_game.py
+++ game/chess_game.py
@@ -1,4 +1,7 @@
+from tkinter.constants import FALSE
+
 from board import Board
+from game.pieces.king import King
 from index_notation import notation_to_index
 
 class ChessGame:
@@ -8,6 +11,9 @@ class ChessGame:
         self.increment = increment
         self.turn = "white"
         self.board = Board()
+        self.white_king = (7, 4)
+        self.black_king = (0, 4)
+        self.result = None
 
     def start_game(self):
         self.board.start_board()
@@ -23,15 +29,42 @@ class ChessGame:
         else:
             try:
                 to_position_index = tuple(notation_to_index(to_position))
-                from_square.move(to_position_index, self.board.board)
+                if type(from_square) == King and from_square.move(to_position_index, self.board.board):
+                    if self.turn == "white":
+                        self.white_king = to_position_index
+                    else:
+                        self.black_king = to_position_index
                 if self.turn == "white":
                     self.turn = "black"
+                    self.game_over("black")
                 else:
                     self.turn = "white"
+                    self.game_over("white")
             except ValueError as ve:
                 print(f"Ошибка формата хода: {ve}")
         self.board.print_board()
 
+    def game_over(self, color: str):
+        if color == "white":
+            i, j = self.white_king
+        else:
+            i, j = self.black_king
+        if not self.board.board[i][j].show_possible_moves().empty():
+            return False
+        for k in range(8):
+            for t in range(8):
+                board_k_t = self.board.board[k][t]
+                if board_k_t is not None:
+                    if board_k_t.color == color and not board_k_t.show_possible_moves().empty():
+                        return False
+
+        if self.board.board[i][j].is_in_check():
+            if color == "white":
+                self.result = "black"
+            else:
+                self.result = "white"
+        else:
+            self.result = "draw"
 
 
         
