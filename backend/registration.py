fc2222be00548c4867c7df3d65379e5cc82d952d
alaloll <alikaloan88@gmail.com>
2024-12-18 05:46:02 +0300

    commit

--- backend/registration.py
+++ backend/registration.py
@@ -31,7 +31,8 @@ origins = [
     "http://127.0.0.1",
     "http://127.0.0.1:8000",
     "http://localhost:8080",
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'http://5.35.5.18',
 ]
 
 app.add_middleware(
