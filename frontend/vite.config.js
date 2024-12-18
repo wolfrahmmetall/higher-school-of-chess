c402a18246f78fa4dd1b21764f4545fa9890a54b
alaloll <alikaloan88@gmail.com>
2024-12-18 03:50:39 +0300

    g

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/higher-school-of-chess-alik",
   plugins: [react()],
+  base: "/higher-school-of-chess-alik/",
   server: {
     proxy: {
       '/chess': {
